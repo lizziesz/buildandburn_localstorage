0d47e565e7f65756c14018c4d2068d1b2a0dac10
Mathew Brown <matt@mattbrown.com>
2016-06-06 09:37:18 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+$(function() {
+  //TODO:When the page loads, change the value of the
+  //input box to what is stored in localstorage.
+  //TODO:When the button is clicked, save the value
+  //in the text box to localstorage.
+});
